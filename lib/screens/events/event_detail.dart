4cb11f01b042d17c38491a7845db1124eaddb960
Youshida90 <YoushidaCompaney@gmail.com>
2024-07-06 15:50:02 +0300

    DTCARD

--- lib/screens/events/event_detail.dart
+++ lib/screens/events/event_detail.dart
@@ -1,7 +1,17 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class EventDetails extends StatefulWidget {
-  const EventDetails({super.key});
+  final String? bannerimage;
+  final String? title;
+  final String? deatials;
+  final String? circleAvatar;
+  const EventDetails({
+    super.key,
+    this.bannerimage,
+    this.title,
+    this.deatials,
+    this.circleAvatar,
+  });
 
   @override
   State<EventDetails> createState() => _EventDetailsState();
@@ -10,6 +20,44 @@ class EventDetails extends StatefulWidget {
 class _EventDetailsState extends State<EventDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          child: Image.network(
+            widget.bannerimage!,
+            fit: BoxFit.fill,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(widget.title!),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          widget.deatials!,
+          maxLines: null,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        SizedBox(
+          height: 50,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) => CircleAvatar(
+              backgroundImage: NetworkImage(widget.circleAvatar!),
+              radius: 30,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
