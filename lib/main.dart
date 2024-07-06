cc7df9fd74f601e12923c3c011f1e1acb56c4908
Abbas Jaber <abbasjaber19@gmail.com>
2024-07-06 17:00:59 +0300

    Abd3 (#8)
    * Open_Screem
    
    * pu
    
    * hh
    
    * account
    
    ---------
    
    Co-authored-by: AbdelhmnAlSharif <Abedelrahmanalsharif31@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:v2/core/feature/color.dart';
 import 'package:v2/remote/providers/home_provider.dart';
 import 'package:v2/screens/auth/login.dart';
+import 'package:v2/screens/events/event_detail.dart';
 import 'package:v2/screens/onboarding/open_screen.dart';
 import 'package:v2/screens/auth/sign_up.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:v2/remote/providers/example_provider.dart';
 import 'package:v2/screens/account_page.dart';
-import 'package:v2/screens/events/event_detail.dart';
 import 'package:v2/screens/home_pages.dart';
 import 'package:v2/screens/main_page.dart';
 import 'package:v2/screens/map_page.dart';
 import 'package:v2/screens/search_page.dart';
+import 'package:v2/widgets/edit_profile.dart';
 import 'remote/di/di_container.dart' as di;
 import 'remote/providers/auth_provider.dart';
 
@@ -36,6 +37,9 @@ class MyApp extends StatelessWidget {
     final searchTabNavigatorKey = GlobalKey<NavigatorState>();
     final mapTabNavigatorKey = GlobalKey<NavigatorState>();
     final accountTabNavigatorKey = GlobalKey<NavigatorState>();
+    final signUpDetailNavigatorKey = GlobalKey<NavigatorState>();
+    final loginDetailNavigatorKey = GlobalKey<NavigatorState>();
+    final editDetailNavigatorKey = GlobalKey<NavigatorState>();
 
     return MaterialApp.router(
         title: 'V2',
@@ -55,28 +59,14 @@ class MyApp extends StatelessWidget {
                 return MainPage(child: navigationShell);
               },
               branches: [
-                StatefulShellBranch(
-                  navigatorKey: homeTabNavigatorKey,
-                  routes: [
-                    GoRoute(
-                      path: '/home',
-                      pageBuilder: (context, state) => const NoTransitionPage(
-                        child: HomePage(),
-                      ),
+                StatefulShellBranch(navigatorKey: homeTabNavigatorKey, routes: [
+                  GoRoute(
+                    path: '/home',
+                    pageBuilder: (context, state) => const NoTransitionPage(
+                      child: HomePage(),
                     ),
-                  ],
-                ),
-                StatefulShellBranch(
-                  navigatorKey: searchTabNavigatorKey,
-                  routes: [
-                    GoRoute(
-                      path: '/search',
-                      pageBuilder: (context, state) => const NoTransitionPage(
-                        child: SearchPage(),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ]),
                 StatefulShellBranch(
                   navigatorKey: mapTabNavigatorKey,
                   routes: [
@@ -131,7 +121,13 @@ class MyApp extends StatelessWidget {
                       //     ? data['product'] as Event
                       //     : null,
                       );
-                })
+                }),
+            GoRoute(
+              path: '/edit_profile',
+              pageBuilder: (context, state) => const NoTransitionPage(
+                child: EditProfile(),
+              ),
+            ),
           ],
         ));
   }
